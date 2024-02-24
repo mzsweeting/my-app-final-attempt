f216e63434c17778b3e779c8b917093db3a6b415
mzsweeting <116032509+mzsweeting@users.noreply.github.com>
2024-02-24 07:02:11 -0500

    Added GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return <div className="App">
+    <h1>Weather App</h1>
+    <footer>
+      This project was coded by: Anntoinette Sweeting and is{" "}
+    <a href= "https://github.com/mzsweeting/my-app-final-attempt.git"
+    target="_blank">
+
+       open-sourced on GitHub
+    </a>
+    </footer>
+    </div>;
 }
 
 
